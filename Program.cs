ef1c073f76c7afecd74a4bfd2fbc682661f32950
SeedPaths-User <lessdismissive@email.com>
2014-09-02 21:08:26 -0600

    Disemvoweler2
    Sep2nd

--- Program.cs
+++ Program.cs
@@ -11,7 +11,11 @@ namespace Disemvoweler
         static void Main(string[] args)
         {
             //Calling Disemvoweler function
-            Disemvoweler("Two drums and a cymbal fall off a cliff");
+            Disemvoweler("Nickleback is my favourite band. Their songwriting can't be beat!");
+
+            Disemvoweler("How many bears could bear grylls grill if bear grylls could grill bears?");
+
+            Disemvoweler("I'm a code ninja, baby. I make the functions and I make the calls.");
         }
 
         static void Disemvoweler(string input)
@@ -23,16 +27,16 @@ namespace Disemvoweler
             for (int i = 0; i < inputLower.Length; i++)
             {
                 //checking if the current letter is a vowel, if so adding to vowels list of strings
-                if("aeiou".Contains(inputLower[i]))
+                if ("aeiou".Contains(inputLower[i]))
                 {
                     vowels.Add(inputLower[i].ToString());
                 }
                 //checking if the current char is a space, if so continue to next value of i. Means not adding to any list.
-                else if(" ".Contains(inputLower[i]))
+                else if (" ".Contains(inputLower[i]))
                 {
                     continue;
                 }
-                    //if it's not vowel and not a space, then add it to output list of strings.
+                //if it's not vowel and not a space, then add it to output list of strings.
                 else
                 {
                     output.Add(inputLower[i].ToString());
@@ -40,13 +44,11 @@ namespace Disemvoweler
             }
 
             //printing the output
+            Console.WriteLine("Original: " + input);
+
+            Console.WriteLine("Disemvoweled: ");
             foreach (var item in output)
-                Console.Write(item.ToUpper());
-
-            Console.WriteLine("\n\nRemoved vowels");
-
-            foreach (var item in vowels)
-                Console.Write(item.ToUpper());
+                Console.Write(item);
 
             Console.WriteLine("\n");
         }
